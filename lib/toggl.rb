d60457a72eb6510fc158837041f3e780d7916958
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2021-09-23 11:40:30 +0900

    initial commit

---
+++ lib/toggl.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "toggl/version"
+
+module Toggl
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/toggl/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Toggl
+  VERSION = "0.1.0"
+end
